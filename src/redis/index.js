11914c82bb93251a6b1187530e94682404122c56
Seyhahaa <jbseyha@gmail.com>
2025-03-19 11:19:21 +0700

    change redis

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,11 +1,11 @@
-const redis = require('redis');
+// const redis = require('redis');
 
-const client = redis.createClient({url: 'redis://event-redis:6379'})
-client.on('connect',()=>{
-    console.log('Connected to Redis')
-})
-client.on('error',(err)=>{
-    console.log('Error connected to Redis',err)
-}).connect()
+// const client = redis.createClient({url: 'redis://event-redis:6379'})
+// client.on('connect',()=>{
+//     console.log('Connected to Redis')
+// })
+// client.on('error',(err)=>{
+//     console.log('Error connected to Redis',err)
+// }).connect()
 
-module.exports = client;
+// module.exports = client;
